39afb0f638f402af620f1b40dea16ac008d58331
marc souchet <marcsouchet@MacBook-Pro-de-marc.local>
2016-11-10 16:00:45 +0100

    second

--- nike/js/NikeBoots.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        autoplay: 3000,
-        loop: true,
-        paginationClickable: true,
-        slidesPerView: 4
-        
-    
-    });
